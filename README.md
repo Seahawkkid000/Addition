8459d02ce6fc42bc41a2961a925a299a712c651b
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2022-12-27 13:43:11 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Addition
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Seahawkkid/pen/YzeZagj](https://codepen.io/Seahawkkid/pen/YzeZagj).
+
---
+++ script.js
@@ -0,0 +1,6 @@
+function add(){
+  var number1=Number(document.getElementById("number1").value)
+  var number2=Number(document.getElementById("number2").value)
+  result=number1+number2
+  window.alert(result)
+}
